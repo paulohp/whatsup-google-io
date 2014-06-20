f2740e0bdd4087d8fe7444c06d6f8b3755a080cc
Paulo Pires <taralloqueiroiz@gmail.com>
2014-06-20 16:56:21 -0300

    socketio

--- app/application.js
+++ app/application.js
@@ -1,4 +1,49 @@
-var app = angular.module('googleIO', []);
+var app = angular.module('googleIO', [
+    'btford.socket-io'
+  ]);
+app.value('nickName', 'anonymous');
+app.factory('mySocket', function (socketFactory) {
+  return socketFactory();
+});
+
+app.controller('chatCtrl',
+  function ($log, $scope, chatSocket, messageFormatter, nickName) {
+  $scope.nickName = nickName;
+  $scope.messageLog = 'Ready to chat!';
+  $scope.sendMessage = function() {
+    var match = $scope.message.match('^\/nick (.*)');
+
+    if (angular.isDefined(match) &&
+        angular.isArray(match) && match.length === 2) {
+      var oldNick = nickName;
+      nickName = match[1];
+      $scope.message = '';
+      $scope.messageLog = messageFormatter(new Date(),
+                      nickName, 'nickname changed - from ' +
+                      oldNick + ' to ' + nickName + '!') +
+                      $scope.messageLog;
+      $scope.nickName = nickName;
+    }
+
+    $log.debug('sending message', $scope.message);
+    chatSocket.emit('message', nickName, $scope.message);
+    $log.debug('message sent', $scope.message);
+    $scope.message = '';
+  };
+  $scope.$on('socket:broadcast', function(event, data) {
+    $log.debug('got a message', event.name);
+    if (!data.payload) {
+      $log.error('invalid message', 'event', event,
+                 'data', JSON.stringify(data));
+      return;
+    }
+    $scope.$apply(function() {
+      $scope.messageLog = messageFormatter(
+            new Date(), data.source,
+            data.payload) + $scope.messageLog;
+    });
+  });
+});  // end of controller
 
 app.directive('googlemaps', function($http) {
     var map,
--- init.js
+++ init.js
@@ -1,6 +1,8 @@
-var express = require('express');
 var fs = require('fs');
+var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 
 app.use(express.static(__dirname + '/'));
